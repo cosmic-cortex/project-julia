cd7747cbafab4e7d3a5274c1343df126975aacd6
cosmic-cortex <theodore.danka@gmail.com>
2019-02-21 17:01:09 +0100

    solution 14 refactored, but slowed down slightly

--- solutions/14-longest_collatz_sequence/solution.jl
+++ solutions/14-longest_collatz_sequence/solution.jl
@@ -3,14 +3,30 @@ function collatz(n)
    return 3*n + 1
 end
 
-global chain_lengths = Dict(1 => 1)
-
-function find_chain_length(n)
-    next = collatz(n)
-    length_at_next = get(chain_lengths, next, 0)
-    length_at_next != 0 && (chain_lengths[n] = length_at_next + 1; return length_at_next + 1)
-    chain_lengths[n] = 1 + find_chain_length(next)
+function cacher(func)
+    cache = Dict()
+    
+    function cached_function(n)
+        val = get(cache, n, 0)
+        val != 0 && return val
+        result = func(n)
+        cache[n] = result
+        return result
+    end
+    
+    return cached_function
 end
 
-@time max_chain_element = argmax([find_chain_length(n) for n=1:1000000])
-println(max_chain_element)
+function find_chain_length(n)
+    n == 1 && return 1
+    return find_chain_length(collatz(n)) + 1
+end
+
+cached_chain_length = cacher(find_chain_length)
+
+function find_longest_chain(upper_limit)
+    return argmax([cached_chain_length(n) for n=1:upper_limit])
+end
+
+@time sol = find_longest_chain(1000000)
+println(sol)
