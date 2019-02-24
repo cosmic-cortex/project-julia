3b9ce5ff5a39d4cc200d1a6fab239abcf217a751
cosmic-cortex <theodore.danka@gmail.com>
2019-02-24 19:12:35 +0100

    memoization in problem 14 fixed

--- solutions/14-longest_collatz_sequence/solution.jl
+++ solutions/14-longest_collatz_sequence/solution.jl
@@ -1,25 +1,18 @@
+upper_limit = 1000000
+
 collatz(n) = n % 2 == 0 ? n ÷ 2 : 3*n + 1
-
-function cacher(func)
-    cache = Dict()
-    
-    function cached_function(n)
-        val = get(cache, n, 0)
-        val != 0 && return val
-        result = func(n)
-        cache[n] = result
-        return result
-    end
-    
-    return cached_function
-end
-
 find_chain_length(n) = n == 1 ? 1 : find_chain_length(collatz(n)) + 1
-cached_chain_length = cacher(find_chain_length)
 
-function find_longest_chain(upper_limit)
-    return argmax([cached_chain_length(n) for n=1:upper_limit])
+@time sol = argmax([find_chain_length(n) for n=1:upper_limit])
+println(sol)
+
+const chain_length = Dict{Int, Int}
+
+function find_chain_length_v2(n)
+    get!(chain_length, n) do n
+        n == 1 ? 1 : find_chain_length(collatz(n)) + 1
+    end
 end
 
-@time sol = find_longest_chain(1000000)
-println(sol)
+@time sol2 = argmax([find_chain_length(n) for n=1:upper_limit])
+println(sol2)
